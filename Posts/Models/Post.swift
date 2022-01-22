d9ca7d2eb0d8b4d98789ac951f37d20751281433
Arvydas Klimavicius <arvydas.klimavicius@gmail.com>
2022-01-22 18:14:58 +0200

    First Commit
    Project setup

---
+++ Posts/Models/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  Posts
+//
+//  Created by Arvydas Klimavicius on 2022-01-20.
+//
+
+import Foundation
---
+++ Posts/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Posts
+//
+//  Created by Arvydas Klimavicius on 2022-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("test")
+    }
+
+
+}
+
